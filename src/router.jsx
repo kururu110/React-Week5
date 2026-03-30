6221fd512c3c1b71cdaad95c1576cc7771814327
kururu110 <kurururu110@gmail.com>
2026-03-30 12:16:00 +0800

    新增所有功能

---
+++ src/router.jsx
@@ -0,0 +1,35 @@
+import { createHashRouter } from "react-router";
+import FrontendLayout from "./layout/FrontendLayout";
+import { Children } from "react";
+import Home from "./views/front/Home";
+import Products from "./views/front/Products";
+import Cart from "./views/front/Cart";
+import SingleProduct from "./views/front/SingleProduct";
+import NotFound from "./views/front/NotFound";
+
+export const router = createHashRouter([
+    {
+        path: "/",
+        element: <FrontendLayout />,
+        children: [{
+            index: true,
+            element: <Home />
+        },
+        {
+            path: "products",
+            element: <Products />
+        },
+        {
+            path: "product/:id",
+            element: <SingleProduct />
+        },
+        {
+            path: "cart",
+            element: <Cart />
+        }]
+    },
+    {
+        path: "*",
+        element: <NotFound />
+    }
+])
---
+++ src/views/front/Home.jsx
@@ -0,0 +1,9 @@
+function Home(params){
+    return(
+        <div>
+            <h1>首頁</h1>
+        </div>
+    )
+}
+
+export default Home;
